8e425c36dc081dd09eec0a3c0a5fe9c9d7b27e3f
Maximilian <maxemsch@freenet.de>
2016-01-02 17:31:57 +0100

    alle benoetigen Dateien dabei

--- 02Parser/02bash_parser_antlr_C/init.sh
+++ 02Parser/02bash_parser_antlr_C/init.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
-export CLASSPATH=~/Downloads/antlr-3.5.2-complete.jar:$CLASSPATH
+export CLASSPATH=../antlr-3.5.2-complete.jar:$CLASSPATH
